27039532d3b32bf0053b89034232855d357ae2e8
Marcelo Smith <marcelosmith77@gmail.com>
2019-02-02 22:12:31 -0200

    Remove alert utilities

--- library/src/main/java/com/marcelosmith77/android/baselibrary/ActivityUtils.java
+++ library/src/main/java/com/marcelosmith77/android/baselibrary/ActivityUtils.java
@@ -4,11 +4,8 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.ContextWrapper;
-import android.content.DialogInterface;
 import android.os.Build;
 import android.support.annotation.Nullable;
-import android.support.annotation.StringRes;
-import android.support.v7.app.AlertDialog;
 
 public class ActivityUtils {
 
@@ -34,149 +31,7 @@ public class ActivityUtils {
     }
 
 
-    /**
-     * Show alert dialog
-     *
-     * @param context - Activity context
-     * @param title - title resource id
-     * @param message - message resource id
-     */
-    public static void showAlert(Context context, int title, int message) {
-
-        showAlert(context, title, message, null);
-    }
-
-    /**
-     * Show alert dialog
-     *
-     * @param context - Activity context
-     * @param title - title resource id
-     * @param message - message resource id
-     * @param onClickListener - on OK button click listener
-     */
-    @SuppressWarnings("WeakerAccess")
-    public static void showAlert(Context context, int title, int message, DialogInterface.OnClickListener onClickListener) {
-
-        if (context == null)
-            return;
-
-        AlertDialog dialog = new AlertDialog.Builder(context, R.style.DialogTheme)
-                .setIcon(R.drawable.ic_alert)
-                .setTitle(title)
-                .setMessage(message)
-                .setCancelable(false)
-                .setPositiveButton(R.string.ok, onClickListener != null ? onClickListener : new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                })
-                .create();
-
-        dialog.show();
-    }
-
-    /**
-     * Show confirmation dialog
-     * @param context - Activity context
-     * @param title - title resource id
-     * @param message - message resource id
-     * @param yesListener - Positive button click listener
-     * @param noListener - Negative button click listener
-     */
-    public static void showConfirm(Context context, int title, int message, DialogInterface.OnClickListener yesListener, DialogInterface.OnClickListener noListener) {
-        showConfirm(context, title, message, R.string.yes, R.string.no, yesListener, noListener);
-    }
-
-    /**
-     * Show confirmation dialog
-     * @param context - Activity context
-     * @param title - title resource id
-     * @param message - message resource id
-     * @param positiveButton - Positive button title
-     * @param negativeButton - Negative button title
-     * @param yesListener - Positive button click listener
-     * @param noListener - Negative button click listener
-     */
-    @SuppressWarnings("WeakerAccess")
-    public static void showConfirm(Context context, @StringRes int title, @StringRes int positiveButton, @StringRes int negativeButton, int message, DialogInterface.OnClickListener yesListener, DialogInterface.OnClickListener noListener) {
-
-        if (context == null)
-            return;
-
-        AlertDialog dialog = new AlertDialog.Builder(context, R.style.DialogTheme)
-                .setIcon(R.drawable.ic_alert)
-                .setTitle(title)
-                .setMessage(message)
-                .setCancelable(false)
-                .setPositiveButton(positiveButton, yesListener != null ? yesListener : new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .setNegativeButton(negativeButton, noListener != null ? noListener : new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .create();
-
-        dialog.show();
-    }
-
-    /**
-     * Show confirmation dialog
-     * @param context - Activity context
-     * @param title - title resource id
-     * @param message - message Text
-     * @param yesListener - Positive button click listener
-     * @param noListener - Negative button click listener
-     */
-    public static void showConfirm(Context context, String title, String message, DialogInterface.OnClickListener yesListener, DialogInterface.OnClickListener noListener) {
-        showConfirm(context, title, message, R.string.yes, R.string.no, yesListener, noListener);
-    }
-
-    /**
-     * Show confirmation dialog
-     * @param context - Activity context
-     * @param title - title resource id
-     * @param message - message resource id
-     * @param positiveButton - Positive button title
-     * @param negativeButton - Negative button title
-     * @param yesListener - Positive button click listener
-     * @param noListener - Negative button click listener
-     */
-    @SuppressWarnings("WeakerAccess")
-    public static void showConfirm(Context context, String title, String message, @StringRes int positiveButton, @StringRes int negativeButton, DialogInterface.OnClickListener yesListener, DialogInterface.OnClickListener noListener) {
-
-        if (context == null)
-            return;
-
-        AlertDialog dialog = new AlertDialog.Builder(context, R.style.DialogTheme)
-                .setIcon(R.drawable.ic_alert)
-                .setTitle(title)
-                .setMessage(message)
-                .setCancelable(false)
-                .setPositiveButton(positiveButton, yesListener != null ? yesListener : new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .setNegativeButton(negativeButton, noListener != null ? noListener : new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .create();
-
-        dialog.show();
-    }
-
-    public static void dismissWithCheck(Dialog dialog) {
+     public static void dismissWithCheck(Dialog dialog) {
         if (dialog != null) {
             if (dialog.isShowing()) {
 
