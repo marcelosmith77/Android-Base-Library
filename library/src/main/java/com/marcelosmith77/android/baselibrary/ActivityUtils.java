dc6bf0a11d2dfe21407bdd090c47c479e8a49cd0
Marcelo Smith <marcelosmith77@gmail.com>
2019-03-05 15:02:21 -0300

    move dismiss dialog utils

--- library/src/main/java/com/marcelosmith77/android/baselibrary/ActivityUtils.java
+++ library/src/main/java/com/marcelosmith77/android/baselibrary/ActivityUtils.java
@@ -31,47 +31,6 @@ public class ActivityUtils {
     }
 
 
-     public static void dismissWithCheck(Dialog dialog) {
-        if (dialog != null) {
-            if (dialog.isShowing()) {
 
-                //get the Context object that was used to great the dialog
-                Context context = ((ContextWrapper) dialog.getContext()).getBaseContext();
-
-                // if the Context used here was an activity AND it hasn't been finished or destroyed
-                // then dismiss it
-                if (context instanceof Activity) {
-
-                    // Api >=17
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                        if (!((Activity) context).isFinishing() && !((Activity) context).isDestroyed()) {
-                            dismissWithTryCatch(dialog);
-                        }
-                    } else {
-
-                        // Api < 17. Unfortunately cannot check for isDestroyed()
-                        if (!((Activity) context).isFinishing()) {
-                            dismissWithTryCatch(dialog);
-                        }
-                    }
-                } else
-                    // if the Context used wasn't an Activity, then dismiss it too
-                    dismissWithTryCatch(dialog);
-            }
-            dialog = null;
-        }
-    }
-
-    public static void dismissWithTryCatch(Dialog dialog) {
-        try {
-            dialog.dismiss();
-        } catch (final IllegalArgumentException e) {
-            // Do nothing.
-        } catch (final Exception e) {
-            // Do nothing.
-        } finally {
-            dialog = null;
-        }
-    }
 }
 
--- library/src/main/java/com/marcelosmith77/android/baselibrary/DialogUtils.java
+++ library/src/main/java/com/marcelosmith77/android/baselibrary/DialogUtils.java
@@ -1,7 +1,11 @@
 package com.marcelosmith77.android.baselibrary;
 
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
 import android.support.annotation.StringRes;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -85,6 +89,49 @@ public class DialogUtils {
                 .onPositive(positiveCallback).build();
 
         dialog.show();
+    }
+
+    public static void dismissWithCheck(Dialog dialog) {
+        if (dialog != null) {
+            if (dialog.isShowing()) {
+
+                //get the Context object that was used to great the dialog
+                Context context = ((ContextWrapper) dialog.getContext()).getBaseContext();
+
+                // if the Context used here was an activity AND it hasn't been finished or destroyed
+                // then dismiss it
+                if (context instanceof Activity) {
+
+                    // Api >=17
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                        if (!((Activity) context).isFinishing() && !((Activity) context).isDestroyed()) {
+                            dismissWithTryCatch(dialog);
+                        }
+                    } else {
+
+                        // Api < 17. Unfortunately cannot check for isDestroyed()
+                        if (!((Activity) context).isFinishing()) {
+                            dismissWithTryCatch(dialog);
+                        }
+                    }
+                } else
+                    // if the Context used wasn't an Activity, then dismiss it too
+                    dismissWithTryCatch(dialog);
+            }
+            dialog = null;
+        }
+    }
+
+    public static void dismissWithTryCatch(Dialog dialog) {
+        try {
+            dialog.dismiss();
+        } catch (final IllegalArgumentException e) {
+            // Do nothing.
+        } catch (final Exception e) {
+            // Do nothing.
+        } finally {
+            dialog = null;
+        }
     }
 
 }
